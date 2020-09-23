2b7f3afbdde2631fbd870cd425174a84240c2abe
Tatiana <t.demonova@gmail.com>
2020-09-22 20:54:28 -0400

    Add button Less functionality

--- script.js
+++ script.js
@@ -29,6 +29,26 @@ document.getElementById('btnOver').addEventListener('click', function () {
     }
 })
 
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber  - 1;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        }
+    }
+})
+
 
 
 document.getElementById('btnEqual').addEventListener('click', function () {
